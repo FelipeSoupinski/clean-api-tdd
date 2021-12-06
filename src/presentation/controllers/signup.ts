714469820cb1059ba679da8e258dbaa2441b0c4a
FelipeSoupinski <sssoupinski@gmail.com>
2021-12-06 14:05:33 -0400

    refactor: create badRequest helper and missing param error

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,14 @@
 import { HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { MissingParamError } from '@/presentation/errors'
+import { badRequest } from '@/presentation/helpers'
 
 export class SignupController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    return {
-      statusCode: 400
+    for (const field of ['name', 'email', 'password', 'password_confirmation']) {
+      console.log(field)
+      if (!httpRequest[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
