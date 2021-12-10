baef292c2ac21bab935b2a38787329812caf6fc0
FelipeSoupinski <sssoupinski@gmail.com>
2021-12-09 20:35:16 -0400

    feat: AddAccountRepository and MongoDb tests and implementation

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -1,6 +1,13 @@
-import { AddAccountModel } from './add-account-model'
-import { AccountModel } from '../models'
-
 export interface AddAccount {
-  add: (account: AddAccountModel) => Promise<AccountModel>
+  add: (account: AddAccount.Params) => Promise<AddAccount.Result>
+}
+
+export namespace AddAccount {
+  export type Params = {
+    name: string
+    email: string
+    password: string
+  }
+
+  export type Result = boolean
 }
---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,10 @@
+import { MongoHelper } from '@/infra/db/mongodb/mongo-helper'
+import { AddAccountRepository } from '@/data/protocols'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (data: AddAccountRepository.Params): Promise<AddAccountRepository.Result> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(data)
+    return result.insertedId !== null
+  }
+}
